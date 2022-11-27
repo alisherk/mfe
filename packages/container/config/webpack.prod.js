ff14a2292f15f9767e576f530c99f1fa6bf21b2d
Alisher Kabildjanov <alisherkabildjanov@yahoo.com>
2022-11-27 17:13:35 -0600

    set publicPath in webpack prod config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js', //ensures dyname name and hash for caching
+    filename: '[name].[contenthash].js', //ensures dynamic name and hash for caching
+    publicPath: '/container/latest/', 
   },
   plugins: [
     new ModuleFederationPlugin({
