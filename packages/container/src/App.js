394b09642c49f28d6e17954140ad00667d87f10e
Alisher Kabildjanov <alisherkabildjanov@yahoo.com>
2022-11-27 17:39:17 -0600

    change workflow config

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1> Hi there 1</h1>
+      <h1> Hi there 2</h1>
       <hr />
       <MarketingApp />
     </div>
