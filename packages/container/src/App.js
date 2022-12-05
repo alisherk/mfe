2e9e401b9f8f47130d60c6a8cd07d7c01dc091e2
Alisher Kabildjanov <alisherkabildjanov@yahoo.com>
2022-12-05 16:46:42 -0600

    dynamic css class generation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({ productionPrefix: 'co' });
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+    <BrowserRouter generateClassName={generateClassName}>
+      <StylesProvider>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
