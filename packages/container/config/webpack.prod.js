379b34d7bda0da8e77eeb58424dc1ccd482b7791
Alisher Kabildjanov <alisherkabildjanov@yahoo.com>
2022-12-20 16:58:20 -0600

    changed prod config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,19 +3,22 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN; 
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js', //ensures dynamic name and hash for caching
-    publicPath: '/container/latest/', 
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
-      remotes: { marketing: `marketing@${domain}/marketing/latest/remoteEntry.js` },
-      shared: packageJson.dependencies
+      remotes: {
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
     }),
   ],
 };
